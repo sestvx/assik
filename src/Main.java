431f8db1d451224ae80d522899c9f7323d8a750a
sestvx <sestvxxx@gmail.com>
2025-02-13 00:10:08 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,105 @@
+import static spark.Spark.*;
+import com.google.gson.Gson;
 import db.FreelancerDAO;
 import db.JobDAO;
 import models.Freelancer;
 import models.Job;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        JobDAO jobDAO = new JobDAO();
+
+        port(8080);
+
+        Gson gson = new Gson();
+
+ 
         FreelancerDAO freelancerDAO = new FreelancerDAO();
-
-        Job newJob = new Job("Java Developer", "Build web applications");
-        jobDAO.createJob(newJob);
-
-        Freelancer newFreelancer = new Freelancer("John Doe", "john.doe@example.com", "Java, Spring Boot, MySQL");
-        freelancerDAO.createFreelancer(newFreelancer);
+        JobDAO jobDAO = new JobDAO();
 
 
-        Job job = jobDAO.readJob(1);
-        System.out.println("Read Job: " + job);
+        get("/freelancers", (req, res) -> {
+            res.type("application/json");
 
-        Freelancer freelancer = freelancerDAO.readFreelancer(1);
-        System.out.println("Read Freelancer: " + freelancer);
+            return gson.toJson(freelancerDAO.listFreelancers());
+        });
 
-        
-        jobDAO.deleteJob(1);
+        get("/freelancers/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            Freelancer freelancer = freelancerDAO.readFreelancer(id);
+            if (freelancer != null) {
+                return gson.toJson(freelancer);
+            } else {
+                res.status(404);
+                return gson.toJson("Freelancer not found");
+            }
+        });
 
-        freelancerDAO.deleteFreelancer(1);
 
-        System.out.println("Listing all jobs:");
-        jobDAO.listJobs();
+        post("/freelancers", (req, res) -> {
+            res.type("application/json");
+            Freelancer freelancer = gson.fromJson(req.body(), Freelancer.class);
+            freelancerDAO.createFreelancer(freelancer);
+            return gson.toJson(freelancer);
+        });
 
-        System.out.println("Listing all freelancers:");
-        freelancerDAO.listFreelancers();
+        put("/freelancers/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            Freelancer freelancer = gson.fromJson(req.body(), Freelancer.class);
+            freelancer.setId(id);
+            freelancerDAO.updateFreelancer(freelancer);
+            return gson.toJson(freelancer);
+        });
+
+        delete("/freelancers/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            freelancerDAO.deleteFreelancer(id);
+            return gson.toJson("Freelancer deleted");
+        });
+
+
+        get("/jobs", (req, res) -> {
+            res.type("application/json");
+
+            return gson.toJson(jobDAO.listJobs());
+        });
+
+        get("/jobs/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            Job job = jobDAO.readJob(id);
+            if (job != null) {
+                return gson.toJson(job);
+            } else {
+                res.status(404);
+                return gson.toJson("Job not found");
+            }
+        });
+
+        post("/jobs", (req, res) -> {
+            res.type("application/json");
+            Job job = gson.fromJson(req.body(), Job.class);
+            jobDAO.createJob(job);
+            return gson.toJson(job);
+        });
+
+        put("/jobs/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            Job job = gson.fromJson(req.body(), Job.class);
+            job.setId(id);
+            jobDAO.updateJob(job);
+            return gson.toJson(job);
+        });
+
+        delete("/jobs/:id", (req, res) -> {
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            jobDAO.deleteJob(id);
+            return gson.toJson("Job deleted");
+        });
     }
 }
